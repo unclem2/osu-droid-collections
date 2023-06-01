09f2e802dc246492470b8c27280c9f67986366fd
unclem2 <74730671+unclem2@users.noreply.github.com>
2023-06-01 17:57:35 +0300

    Update 3-downloader.py
    Multithread, collection selection

--- o1d-collector/3-downloader.py
+++ o1d-collector/3-downloader.py
@@ -1,4 +1,5 @@
 import os
+import concurrent.futures
 
 # Очищаем экран
 if os.name == 'nt':
@@ -14,33 +15,73 @@ from tqdm import tqdm
 from termcolor import colored
 
 url = "https://api.chimu.moe/v1/download/"
-json_file = "beatmapset_ids.json"
+collections_dir = "output/collections"
 
 # Создаем директорию для хранения карт
 if not os.path.exists("Songs"):
     os.makedirs("Songs")
 
-with open(json_file, 'r') as f:
-    data = json.load(f)
-    with tqdm(total=len(data), position=0, dynamic_ncols=True) as pbar:
-        for beatmap_id in data:
-            download_url = f"{url}{beatmap_id}"
-            response = requests.get(download_url)
-            if response.ok:
-                filename = unquote(response.headers.get('content-disposition').split('filename=')[1])
-                file_path = os.path.join("Songs", filename)
-                if os.path.isfile(file_path):
-                    pbar.write(colored(f"Карта {filename} уже существует в директории!", "yellow"))
-                    continue
-                elif os.path.isdir(os.path.join("Songs", filename.split('.')[0])):
-                    pbar.write(colored(f"Папка {filename.split('.')[0]} уже существует в директории!", "yellow"))
-                    continue
-                with open(file_path, 'wb') as f:
-                    f.write(response.content)
-                    if os.path.isfile(file_path):
-                        pbar.write(colored(f"Карта {filename} успешно загружена!", "green"))
-                    else:
-                        pbar.write(colored(f"Ошибка записи карты {filename} на диск!", "red"))
+def download_beatmap(beatmap_id):
+    download_url = f"{url}{beatmap_id}"
+    response = requests.get(download_url)
+    if response.ok:
+        filename = unquote(response.headers.get('content-disposition').split('filename=')[1])
+        file_path = os.path.join("Songs", filename)
+        if os.path.isfile(file_path):
+            print(colored(f"Карта {filename} уже существует в директории!", "yellow"))
+            return
+        elif os.path.isdir(os.path.join("Songs", filename.split('.')[0])):
+            print(colored(f"Папка {filename.split('.')[0]} уже существует в директории!", "yellow"))
+            return
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+            if os.path.isfile(file_path):
+                print(colored(f"Карта {filename} успешно загружена!", "green"))
             else:
-                pbar.write(colored(f"Ошибка загрузки карты с ID {beatmap_id}: {response.status_code}", "red"))
-            pbar.update(1)
+                print(colored(f"Ошибка записи карты {filename} на диск!", "red"))
+    else:
+        print(colored(f"Ошибка загрузки карты с ID {beatmap_id}: {response.status_code}", "red"))
+
+# Получаем список доступных коллекций
+collections = []
+for file in os.listdir(collections_dir):
+    file_path = os.path.join(collections_dir, file)
+    if os.path.isfile(file_path) and "beatmapset_list_" not in file:
+        collection_name = file.replace("beatmapset_ids_", "").replace(".json", "")
+        collections.append(collection_name)
+
+# Выводим список коллекций и просим пользователя выбрать
+print(colored("Доступные коллекции:", "cyan"))
+for i, collection in enumerate(collections):
+    print(colored(f"{i + 1}. {collection}", "cyan"))
+
+selected_collection = None
+while selected_collection is None:
+    try:
+        choice = int(input(colored("Выберите номер коллекции для загрузки: ", "yellow")))
+        if 1 <= choice <= len(collections):
+            selected_collection = collections[choice - 1]
+        else:
+            print(colored("Некорректный выбор. Попробуйте снова.", "red"))
+    except ValueError:
+        print(colored("Некорректный выбор. Попробуйте снова.", "red"))
+
+# Загружаем выбранную коллекцию
+collection_file = os.path.join(collections_dir, f"beatmapset_ids_{selected_collection}.json")
+with open(collection_file, 'r') as f:
+    data = json.load(f)
+    if isinstance(data, list):
+        print(colored(f"Загружаем коллекцию: {selected_collection}", "green"))
+        
+        with tqdm(total=len(data), position=0, dynamic_ncols=True) as pbar:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = []
+                for beatmap_id in data:
+                    future = executor.submit(download_beatmap, beatmap_id)
+                    futures.append(future)
+
+                for future in concurrent.futures.as_completed(futures):
+                    pbar.update(1)
+
+    else:
+        print(colored(f"Ошибка: Файл '{selected_collection}' не содержит список beatmap IDs.", "red"))
