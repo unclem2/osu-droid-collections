ca0ac4d12176b88b6391f7cb7e0dea973a1f9722
unclem2 <74730671+unclem2@users.noreply.github.com>
2023-06-02 19:03:38 +0300

    Easy Breezy Easy Breezy Easy Breezy Easy Breezy はい 始まった 絡まった からかったやつらは どっかに いっちゃった 誰に頼まれたわけでもないのに 止まらね～筆 ここでは誰もが王様なのね てか長続きする気がしない とかそんな性格でなにが悪い ただ好きなもんは好き 外野はお黙り キミ無責任 ここ地球 ゼログラビティ うっさいなあ ジャマしないでね イッパツ ハデにやってみようや 脳みそ イマジネーション 見たもん 聞いたもん それ全部 血になる 飛べる 飛べ! なめさせとけ 頭ん中 もう完成形見えた 頭ん中 もう自由自在 心の網目 細かくして 最強の世界できちゃってるわな～ Easy Breezy Easy Breezy Easy Breezy Easy Breezy 一張羅 パリッと着込んで 戦闘モードなんだこりゃ どうせやるならめんどくさくなろうぜ なるようにするのさ Easy Breezy いいもんさ 覚悟決めたなら やったりゃいいじゃん どんなもんだ! やぶれかぶれでもどーにかこーにかしようぜ なるようにするのさ Easy Breezy Easy Breezy Easy Breezy Easy Breezy Easy Breezy Breezy, Breezy, Breezy, Breezy, Breezy, Breezy Breezy, Breezy, Breezy, Breezy, Breezy, Breezy Breezy, Breezy, Breezy, Breezy 見切り発車で 進んじまったね 街の灯りが 後ろに消えてく孤独 手ぶらでも なるようにするのさ 笑 あら もうこんな時間? 腹減っためっちゃ ラーメン食べちゃう I say hi you say hi もうちょっと待ってな あっちゅーま 完成する なんて意外 窓開けな 鳥 鳴いてら こっちへおいでよ 一面からじゃみせらんない こっちへおいでよ ちゃんと自分で意識してみ ほらどんなもん描こうか こんな色はどうだ こいつらいれば ヨユーだわ 黄ばんだ目のやつら ばっちいわ ちな 来たら追っ払うわ モーマンタイ 無問題 あーしらはかわんない とまるつもりないわ Easy Breezy Easy Breezy Easy Breezy Easy Breezy 一張羅 パリッと着込んで 戦闘モードなんだこりゃ どうせやるならめんどくさくなろうぜ なるようにするのさ Easy Breezy いいもんさ 覚悟決めたなら やったりゃいいじゃん どんなもんだ! 答え分かってりゃ苦労なんかしないぜ なるようにするのさ Easy Breezy Easy Breezy Easy Breezy Easy Breezy Easy Breezy

--- o1d-collector/duplicate_cleaner.py
+++ o1d-collector/duplicate_cleaner.py
@@ -1,33 +1,54 @@
 import os
 import shutil
 from tkinter import filedialog
+import re
 
 # Выберите путь к папке, которую нужно проверить
 path = filedialog.askdirectory()
 
-# Проверьте, существует ли папка "duplicates", и если она не существует, создайте ее
-if not os.path.exists(os.path.join(path, "duplicates")):
-    os.makedirs(os.path.join(path, "duplicates"))
+# Функция для удаления папки с самым длинным названием
+def delete_longest_folder(folder_list):
+    if len(folder_list) <= 1:
+        return
 
-# Получите список всех папок в указанной директории
-folders = [folder for folder in os.listdir(path) if os.path.isdir(os.path.join(path, folder))]
+    longest_folder = max(folder_list, key=lambda x: len(x))
+    folder_path = longest_folder
+    shutil.rmtree(folder_path)
+    print(f"Папка {folder_path} была удалена")
 
-# Создайте словарь, в котором ключ - первые 7 символов названия папки, а значение - список папок с таким названием
-folder_dict = {}
-for folder in folders:
-    key = folder[:7]
-    if key in folder_dict:
-        folder_dict[key].append(folder)
-    else:
-        folder_dict[key] = [folder]
+# Создание словаря с первыми 7 символами названий папок, содержащих цифры
+folder_dict_7 = {}
 
-# Пройдитесь по каждой паре папок с одинаковым названием и переместите ту, которая короче, в папку "duplicates"
-for key in folder_dict:
-    if len(folder_dict[key]) > 1:
-        shortest_folder = min(folder_dict[key], key=len)
-        for folder in folder_dict[key]:
-            if folder == shortest_folder:
-                shutil.move(os.path.join(path, folder), os.path.join(path, "duplicates", folder))
-                print(f"Папка {folder} была перемещена в папку duplicates")
+# Проход по всем папкам и подпапкам для поиска папки "Songs" и удаления дубликатов
+for root, dirs, files in os.walk(path):
+    if "Songs" in dirs:
+        songs_folder = os.path.join(root, "Songs")
 
-print("Процесс завершен!")
+        # Получение списка всех папок в папке "Songs"
+        folders = [folder for folder in os.listdir(songs_folder) if os.path.isdir(os.path.join(songs_folder, folder))]
+
+        # Заполнение словаря с первыми 7 символами названий папок, содержащих цифры
+        for folder in folders:
+            folder_path = os.path.join(songs_folder, folder)
+            folder_name = re.search(r'\d+', folder)
+            if folder_name:
+                key_7 = folder_name.group()[:7]
+                if key_7 in folder_dict_7:
+                    folder_dict_7[key_7].append(folder_path)
+                else:
+                    folder_dict_7[key_7] = [folder_path]
+
+        # Проверка наличия дубликатов в словаре с первыми 7 символами названий папок
+        duplicate_count = 0
+        for key_7 in folder_dict_7:
+            folder_list = folder_dict_7[key_7]
+            if len(folder_list) > 1:
+                duplicate_count += 1
+                print(f"Найдено совпадение по первым 7 символам, содержащим цифры: {key_7}")
+                delete_longest_folder(folder_list)
+
+        print("Процесс завершен!")
+        print(f"Всего найдено дубликатов: {duplicate_count}")
+        exit()
+
+print("Папка 'Songs' не найдена. Скрипт завершен.")
