ba7b45024c40ff4401e9abe0218b4c4ca691503e
unclem2 <74730671+unclem2@users.noreply.github.com>
2023-06-03 15:19:09 +0300

    исправил пути для мергера

--- o1d-collector/jsonmerger.py
+++ o1d-collector/jsonmerger.py
@@ -14,8 +14,11 @@ def merge_favorite_scripts(root_directory):
                 file_paths.append(file_path)  # Добавляем путь к файлу в список
     return data, file_paths
 
-# Ввод пути к корневой директории через консоль
-root_directory = input("Введите путь к корневой директории: ")
+# Получаем текущую директорию скрипта
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Определяем корневую директорию как папку ниже текущей директории скрипта
+root_directory = os.path.join(script_directory, "..", "..")
 
 # Вызываем функцию для объединения скриптов и получения путей файлов
 merged_data, file_paths = merge_favorite_scripts(root_directory)
@@ -28,9 +31,9 @@ for file_path in file_paths:
 # Подтверждение сохранения файла
 confirm = input("Хотите сохранить объединенный файл? (yes/no): ")
 if confirm.lower() == "yes":
-    # Ввод пути для сохранения файла через консоль
-    save_directory = input("Введите путь для сохранения файла: ")
-    save_path = os.path.join(save_directory, "merged_favorite.json")
+    # Ввод пути для сохранения файла 
+    save_directory = root_directory
+    save_path = os.path.join(save_directory, 'json', "merged_favorite.json")
 
     # Записываем объединенные данные в новый файл
     with open(save_path, 'w') as merged_file:
