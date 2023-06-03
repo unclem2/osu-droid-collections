ed19cb23fcd160f64a0eae9733ebb438de583dbb
unclem2 <74730671+unclem2@users.noreply.github.com>
2023-06-03 12:26:48 +0300

    ssvd

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 pkg install python
 pip install -r requirements.txt
-cd ..
+
 termux-setup-storage
 cp ~/osu-droid-collections -r ~/storage/shared/osu\!droid/ 
-cd storage/shared/osu\!droid
+cd ~/storage/shared/osu\!droid/
