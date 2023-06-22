b52962fd113874680500a1a989e4720e2e111cb8
unclem2 <74730671+unclem2@users.noreply.github.com>
2023-06-22 18:30:25 +0300

    шо

--- o1d-collector/downloader.py
+++ o1d-collector/downloader.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from termcolor import colored
 import threading
 
-# Очищаем экран
+# Clear the screen
 if os.name == 'nt':
     os.system('cls')
 else:
@@ -22,17 +22,17 @@ def find_songs_directory(directory):
             return os.path.join(root, "Songs")
     return None
 
-# Определение директории скрипта
+# Determine the script directory
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Поиск папки "Songs" на два уровня выше директории скрипта
+# Search for the "Songs" folder two levels above the script directory
 parent_directory = os.path.abspath(os.path.join(script_directory, ".."))
 songs_dir = find_songs_directory(parent_directory)
 if songs_dir is None:
     parent_directory = os.path.abspath(os.path.join(parent_directory, ".."))
     songs_dir = find_songs_directory(parent_directory)
 
-# Создание папки "Songs", если не найдена
+# Create the "Songs" folder if not found
 if songs_dir is None:
     songs_dir = os.path.join(parent_directory, "Songs")
     os.makedirs(songs_dir)
@@ -44,21 +44,21 @@ def download_beatmap(beatmap_id):
         filename = unquote(response.headers.get('content-disposition').split('filename=')[1])
         file_path = os.path.join(songs_dir, filename)
         if os.path.isfile(file_path):
-            print(colored(f"Карта {filename} уже существует в директории!", "yellow"))
+            print(colored(f"The beatmap {filename} already exists in the directory!", "yellow"))
             return
         elif os.path.isdir(os.path.join(songs_dir, filename.split('.')[0])):
-            print(colored(f"Папка {filename.split('.')[0]} уже существует в директории!", "yellow"))
+            print(colored(f"The folder {filename.split('.')[0]} already exists in the directory!", "yellow"))
             return
         with open(file_path, 'wb') as f:
             f.write(response.content)
             if os.path.isfile(file_path):
-                print(colored(f"Карта {filename} успешно загружена!", "green"))
+                print(colored(f"The beatmap {filename} was successfully downloaded!", "green"))
             else:
-                print(colored(f"Ошибка записи карты {filename} на диск!", "red"))
+                print(colored(f"Failed to write the beatmap {filename} to disk!", "red"))
     else:
-        print(colored(f"Ошибка загрузки карты с ID {beatmap_id}: {response.status_code}", "red"))
+        print(colored(f"Failed to download beatmap with ID {beatmap_id}: {response.status_code}", "red"))
 
-# Получаем список доступных коллекций
+# Get the list of available collections
 collections = []
 for file in os.listdir(collections_dir):
     file_path = os.path.join(collections_dir, file)
@@ -66,38 +66,38 @@ for file in os.listdir(collections_dir):
         collection_name = file.replace("beatmapset_ids_", "").replace(".json", "")
         collections.append(collection_name)
 
-# Выводим список коллекций и просим пользователя выбрать
-print(colored("Доступные коллекции:", "cyan"))
+# Display the list of collections and prompt the user to choose
+print(colored("Available collections:", "cyan"))
 for i, collection in enumerate(collections):
     print(colored(f"{i + 1}. {collection}", "cyan"))
 
 selected_collection = None
 while selected_collection is None:
     try:
-        choice = int(input(colored("Выберите номер коллекции для загрузки: ", "yellow")))
+        choice = int(input(colored("Select the collection number to download: ", "yellow")))
         if 1 <= choice <= len(collections):
             selected_collection = collections[choice - 1]
         else:
-            print(colored("Некорректный выбор. Попробуйте снова.", "red"))
+            print(colored("Invalid choice. Please try again.", "red"))
     except ValueError:
-        print(colored("Некорректный выбор. Попробуйте снова.", "red"))
+        print(colored("Invalid choice. Please try again.", "red"))
 
-# Загружаем выбранную коллекцию
+# Download the selected collection
 collection_file = os.path.join(collections_dir, f"beatmapset_ids_{selected_collection}.json")
 with open(collection_file, 'r') as f:
     data = json.load(f)
     if isinstance(data, list):
-        print(colored(f"Загружаем коллекцию: {selected_collection}", "green"))
+        print(colored(f"Downloading collection: {selected_collection}", "green"))
 
-        stop_event = threading.Event()  # Событие для остановки загрузки
+        stop_event = threading.Event()  # Event for stopping the download
 
         def check_user_input():
             while True:
                 if input() or stop_event.is_set():
                     break
-            stop_event.set()  # Установка события остановки
+            stop_event.set()  # Set the stop event
 
-        # Запускаем поток для проверки ввода пользователя
+        # Start a thread to check user input
         input_thread = threading.Thread(target=check_user_input)
         input_thread.start()
 
@@ -106,7 +106,7 @@ with open(collection_file, 'r') as f:
                 futures = []
                 for beatmap_id in data:
                     if stop_event.is_set():
-                        break  # Прерываем цикл загрузки, если событие остановки установлено
+                        break  # Break the download loop if the stop event is set
                     future = executor.submit(download_beatmap, beatmap_id)
                     futures.append(future)
 
@@ -115,12 +115,12 @@ with open(collection_file, 'r') as f:
                     pbar.update(1)
                     completed += 1
                     if completed >= len(futures):
-                        stop_event.set()  # Установка события остановки после завершения всех задач
+                        stop_event.set()  # Set the stop event after completing all tasks
 
-        # Ожидаем завершения потока проверки ввода
+        # Wait for the input thread to finish
         input_thread.join()
 
     else:
-        print(colored(f"Ошибка: Файл '{collection_file}' не содержит список beatmap IDs.", "red"))
+        print(colored(f"Error: The file '{collection_file}' does not contain a list of beatmap IDs.", "red"))
 
-print(colored("Скрипт завершен.", "cyan"))
+print(colored("Script completed.", "cyan"))
--- o1d-collector/jsonmerger.py
+++ o1d-collector/jsonmerger.py
@@ -3,7 +3,7 @@ import json
 
 def merge_favorite_scripts(root_directory):
     data = {}
-    file_paths = []  # Список путей к объединяемым файлам
+    file_paths = []  # List of paths to the merged files
     for root, dirs, files in os.walk(root_directory):
         for file in files:
             if file == "favorite.json":
@@ -11,33 +11,33 @@ def merge_favorite_scripts(root_directory):
                 with open(file_path, 'r') as f:
                     favorite_data = json.load(f)
                     data.update(favorite_data)
-                file_paths.append(file_path)  # Добавляем путь к файлу в список
+                file_paths.append(file_path)  # Add the file path to the list
     return data, file_paths
 
-# Получаем текущую директорию скрипта
+# Get the current script directory
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Определяем корневую директорию как папку ниже текущей директории скрипта
+# Set the root directory as a folder below the current script directory
 root_directory = os.path.join(script_directory, "..", "..")
 
-# Вызываем функцию для объединения скриптов и получения путей файлов
+# Call the function to merge scripts and get the file paths
 merged_data, file_paths = merge_favorite_scripts(root_directory)
 
-# Выводим пути файлов, которые будут объединены
-print("Обнаружены следующие файлы для объединения:")
+# Display the file paths that will be merged
+print("The following files will be merged:")
 for file_path in file_paths:
     print(file_path)
 
-# Подтверждение сохранения файла
-confirm = input("Хотите сохранить объединенный файл? (yes/no): ")
+# Confirm file saving
+confirm = input("Do you want to save the merged file? (yes/no): ")
 if confirm.lower() == "yes":
-    # Ввод пути для сохранения файла 
+    # Enter the save path for the file
     save_directory = root_directory
     save_path = os.path.join(save_directory, 'json', "merged_favorite.json")
 
-    # Записываем объединенные данные в новый файл
+    # Write the merged data to a new file
     with open(save_path, 'w') as merged_file:
         json.dump(merged_data, merged_file, indent=4)
-    print("Файл успешно сохранен по пути:", save_path)
+    print("File successfully saved at:", save_path)
 else:
-    print("Сохранение отменено.")
+    print("Save canceled.")
